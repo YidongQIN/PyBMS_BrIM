59effc60765393e3157f3aac00662658017ecd5e
YidongQIN <YidongQIN@users.noreply.github.com>
2017-12-03 11:43:15 -0500

    comment to test Git in PyCharm

--- 1-ET.py
+++ 1-ET.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 tree = ET.parse('0 MARC.xml')
 root = tree.getroot()
 
+# find all the child element
 marc_object=root.findall('O')
 for a in marc_object:
 	print(a.attrib)
