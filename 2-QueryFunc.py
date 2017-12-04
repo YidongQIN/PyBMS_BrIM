431308ea4c8ed3d8be167f74848e53e13b63d48b
YidongQIN <YidongQIN@users.noreply.github.com>
2017-12-04 17:05:27 -0500

    @Mason

--- 2-QueryFunc.py
+++ 2-QueryFunc.py
@@ -1,15 +1,22 @@
-# import the ET API
-
 import xml.etree.ElementTree as ET
-tree = ET.parse('0 MARC.xml')
+# XML file
+## read XML
+def read_xml(in_path):
+	tree = ET.parse(in_path)
+	return tree
+## write XML
+def write_xml(tree, out_path):
+	tree.write(out_path, encoding="utf-8",xml_declaration=True)
+# search
+##
+# change
+# ----main----
+
+tree = read_xml('0 MARC.xml')
 root = tree.getroot()
 
-print('---root.get---')
-print(root.get('N'))
-print('---root.items---')
-a = root.items()
-print(a[1])
-print('---root.attrib---')
-b = root.attrib
-print(b['T'])
+a = root.iter('O')
+for b in a:
+	if 'Type' in b.keys():
+		print(b.get('Type'))
 
