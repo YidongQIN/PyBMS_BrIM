18bca050930e837574686cc81e68171f932b7dbe
YidongQIN <YidongQIN@users.noreply.github.com>
2017-12-08 12:23:08 -0500

    @Mason 1206

--- 2-QueryFunc.py
+++ 2-QueryFunc.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ET
 
-# ParamML XML file
 ## read XML
 def read_xml(in_path):
 	tree = ET.parse(in_path)
@@ -9,29 +8,48 @@ def read_xml(in_path):
 def write_xml(tree, out_path):
 	tree.write(out_path, encoding="utf-8",xml_declaration=True)
 # search
-##
+## search by key and value of attributes
 def if_match(node, kv_map):
-    '''''判断某个节点是否包含所有传入参数属性
-       node: 节点
-       kv_map: 属性及属性值组成的map'''
-    for key in kv_map:
-        if node.get(key) != kv_map.get(key):
-            return False
-    return True
-
+	for key in kv_map:
+		if node.get(key) != kv_map.get(key):
+			return False
+	return True
+#
 def get_node_by_keyvalue(nodelist, kv_map):
-    '''根据属性及属性值定位符合的节点，返回节点
-       nodelist: 节点列表
-       kv_map: 匹配属性及属性值map'''
-    result_nodes = []
-    for node in nodelist:
-        if if_match(node, kv_map):
-            result_nodes.append(node)
-    return result_nodes
+	result_nodes = []
+	for node in nodelist:
+		if if_match(node, kv_map):
+			result_nodes.append(node)
 
+	return result_nodes
+# search by node path
+def find_nodes(tree, path):
+	return tree.findall(path)
+# output format
+def O_or_P(op):
+	if op is'O':
+		return 'Object'
+	elif op is 'P' :
+		return 'Parameter'
+	else:
+		return 'Not correct tag'
+
+def attrib(dict):
+	outformat=[]
+	for eachkey in dict:
+		a=str[eachkey.key]+'='+str[eachkey.value]
+		outformat.append[a]
+	return outformat
 # ----main----
-tree = read_xml('0 MARC.xml')
-root = tree.getroot()
+if __name__ == "__main__":
+
+	tree = read_xml('0 MARC.xml')
+	root = tree.getroot()
+	# input and search path
+	path = input('input the path\n')
+	results = find_nodes(root, path)
 
 
-
+	# output results
+	for anode in results:
+		print(attrib(anode.attrib))
