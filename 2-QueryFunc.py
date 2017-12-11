6d758d6fc30e60e2bdf0f46c072ca28f32372b8a
YidongQIN <qinyidong87@outlook.com>
2017-12-11 18:04:41 -0500

    home

--- 2-QueryFunc.py
+++ 2-QueryFunc.py
@@ -9,30 +9,21 @@ def read_xml(in_path):
 def write_xml(tree, out_path):
 	tree.write(out_path, encoding="utf-8",xml_declaration=True)
 # search
-##
+## search by attrib
 def if_match(node, kv_map):
-    '''''判断某个节点是否包含所有传入参数属性
-       node: 节点
-       kv_map: 属性及属性值组成的map'''
     for key in kv_map:
         if node.get(key) != kv_map.get(key):
             return False
     return True
 
 def get_node_by_keyvalue(nodelist, kv_map):
-    '''根据属性及属性值定位符合的节点，返回节点
-       nodelist: 节点列表
-       kv_map: 匹配属性及属性值map'''
     result_nodes = []
     for node in nodelist:
         if if_match(node, kv_map):
             result_nodes.append(node)
     return result_nodes
-#
+## search by path
 def find_nodes(tree, path):
-    '''查找某个路径匹配的所有节点
-       tree: xml树
-       path: 节点路径'''
     return tree.findall(path)
 # ----main----
 tree = read_xml('0 MARC.xml')
