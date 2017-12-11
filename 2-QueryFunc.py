c80d9f7543df8818b81899c543b53e07278384b4
YidongQIN <qinyidong87@outlook.com>
2017-12-11 18:08:40 -0500

    Modify Query

--- 2-QueryFunc.py
+++ 2-QueryFunc.py
@@ -5,44 +5,31 @@ import json
 def read_xml(in_path):
     tree = ET.parse(in_path)
     return tree
-
-
 ## write XML
 def write_xml(tree, out_path):
     tree.write(out_path, encoding="utf-8", xml_declaration=True)
-
-
 # search
-<<<<<<< HEAD
-## search by attrib
-=======
 ## search by key and value of attributes
->>>>>>> b2ba57f0a47f3c53f11e10556c457af4af1c6df8
 def if_match(node, kv_map):
     for key in kv_map:
         if node.get(key) != kv_map.get(key):
             return False
     return True
 
-
-#
 def get_node_by_keyvalue(nodelist, kv_map):
     result_nodes = []
     for node in nodelist:
         if if_match(node, kv_map):
             result_nodes.append(node)
     return result_nodes
-<<<<<<< HEAD
+
 ## search by path
 def find_nodes(tree, path):
     return tree.findall(path)
-=======
-
 
 # search by node path
 def find_nodes(tree, path):
     return tree.findall(path)
-
 
 # output format
 def O_or_P(op):
@@ -53,21 +40,19 @@ def O_or_P(op):
     else:
         return 'Not correct tag'
 
->>>>>>> b2ba57f0a47f3c53f11e10556c457af4af1c6df8
 # ----main----
 
 tree = read_xml('0 MARC.xml')
 root = tree.getroot()
 # input and search path
 
-<<<<<<< HEAD
-=======
 # path = input('input the path\n')
 # result = find_nodes(root, path)
 
 # input and search by attrib
 op = input('Object or Parameter? Type "O" or "P" please.\n')
 # kv = input('Input attributes in format of dict \{ \}')
+op = 'O'
 kv = {"T":"Node", "Y":"0"}
 nodes = root.iter(op)
 results = get_node_by_keyvalue(nodes, kv)
@@ -75,4 +60,3 @@ results = get_node_by_keyvalue(nodes, kv)
 for anode in results:
     op = O_or_P(anode.tag)
     print('Tag is', op, 'and attribute is', anode.attrib)
->>>>>>> b2ba57f0a47f3c53f11e10556c457af4af1c6df8
