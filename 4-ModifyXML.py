bd5025394e5f49818fb1b9fceb7f4d1e0ef1807b
YidongQIN <YidongQIN@users.noreply.github.com>
2017-12-12 15:23:13 -0500

    Search and Modify func

--- 4-ModifyXML.py
+++ 4-ModifyXML.py
@@ -1,45 +1,40 @@
 import xml.etree.ElementTree as ET
 import prettytable as pt
 
+
 # read XML
 def read_xml(in_path):
     tree = ET.parse(in_path)
     return tree
+
+
 # write XML
 def write_xml(tree, out_path):
     tree.write(out_path, encoding="utf-8", xml_declaration=True)
+
+
 # search by path
 def find_nodes(tree, path):
     return tree.findall(path)
-## search by key and value of attributes
+
+
+# search by key and value of attributes
 def if_match(node, kv_map):
     for key in kv_map:
         if node.get(key) != kv_map.get(key):
             return False
     return True
+
+
 def get_node_by_keyvalue(nodelist, kv_map):
     result_nodes = []
     for node in nodelist:
         if if_match(node, kv_map):
             result_nodes.append(node)
     return result_nodes
-# output format
-def O_or_P(op):
-    if op is 'O':
-        return 'Object'
-    elif op is 'P':
-        return 'Parameter'
-    else:
-        return 'Not correct tag'
-def other_attribute(dict):
-    dict.pop('N')
-    dict.pop('T')
-    atts = ''
-    for key, value in dict.items():
-        atts = atts + key + '=' + value + ', '
-    return atts
 
-# ----change----
+
+# change node
 def change_node_properties(nodelist, kv_map, is_delete=False):
     for node in nodelist:
         for key in kv_map:
@@ -49,56 +44,122 @@ def change_node_properties(nodelist, kv_map, is_delete=False):
             else:
                 node.set(key, kv_map.get(key))
 
-def create_node(tag, property_map):
-    element = ET.Element(tag, property_map)
+
+# creat and add a new node
+def create_node(tag, attribute_new):
+    element = ET.Element(tag, attribute_new)
     return element
+
 
 def add_child_node(nodelist, element):
     for node in nodelist:
         node.append(element)
 
+
+# delete a node by attribute
 def del_node_by_tagkeyvalue(nodelist, tag, kv_map):
-    '''同过属性及属性值定位一个节点，并删除之
-       nodelist: 父节点列表
-       tag:子节点标签
-       kv_map: 属性及属性值列表'''
     for parent_node in nodelist:
-        children = parent_node.getchildren()
+        children = parent_node.iter()
         for child in children:
             if child.tag == tag and if_match(child, kv_map):
                 parent_node.remove(child)
+
+# seperate OBJECT and PARAMETER
+def select_OBJECT(nodelist):
+    node_O = ET.Element("", {})
+    for node in nodelist:
+        if node.tag is 'O':
+            node_O.append(node)
+    return node_O
+
+
+def select_PARAMETER(nodelist):
+    node_P = ET.Element("", {})
+    for node in nodelist:
+        if node.tag is 'P':
+            node_P.append(node)
+    return node_P
+
+# T -- main attribute for OBJECT
+# N, V -- main attribute for PARAMETER
+def other_attribute(dict):
+    if 'T' in dict:
+        dict.pop('T')
+    if 'N' in dict:
+        dict.pop('N')
+    if 'V' in dict:
+        dict.pop('V')
+    atts = ''
+    for key, value in dict.items():
+        atts = atts + key + '=' + value + ', '
+    return atts
+
+
+# pretty table
+def table_OBJECT(result_Object):
+    tb = pt.PrettyTable(["Name", "OBJECT Type", "Other Attributes"])
+    tb.align["Other Attributes"] = "l"
+    for anode in result_Object:
+        row = [anode.attrib.get("N"), anode.attrib.get("T"), other_attribute(anode.attrib)]
+        tb.add_row(row)
+    print('\n Table of OBJECT found')
+    print(tb)
+
+def table_PARAMETER(result_Parameter):
+    tb = pt.PrettyTable(["Name", "Value", "Description", "Other Attributes"])
+    tb.align["Other Attributes"] = "l"
+    for anode in result_Parameter:
+        row = [anode.attrib.get("N"), anode.attrib.get("V"), anode.attrib.get("D"), other_attribute(anode.attrib)]
+        tb.add_row(row)
+    print('\n Table of PARAMETER found')
+    print(tb)
+    # return tb
+
 
 # ----main----
 
 tree = read_xml('0 MARC.xml')
 root = tree.getroot()
 
-# input and search path
-# path = input('input the path\n')
-# result = find_nodes(root, path)
-
-# input and search by attrib
-# op = input('Object or Parameter? Type "O" or "P" in.\n')
-# kv = input('Input attributes in format of dict {"key":"value",}')
-
-# test
-op = "O"
-kv = {"T":"Material"}
-
 # find the node
-nodes = root.iter(op)
-results = get_node_by_keyvalue(nodes, kv)
+search_method = input('Search by 1.Path or 2.Attributes?\n')
+if search_method is '1':
+    path='./O/P'
+    # path = input('input the path in Xpath format\n')
+    results = find_nodes(root, path)
+else:
+    # op = input('Object or Parameter? Type "O" or "P"\n')
+    # kv = input('Input attributes in format of dict {"key":"value",}')
+    op = "*"
+    kv = {"T": "Material"}
+    # kv = {"D": "modulus of elasticity"}
+    results = get_node_by_keyvalue(root.iter(op), kv)
+
+if select_OBJECT(results):
+    table_OBJECT(select_OBJECT(results))
+if select_PARAMETER(results):
+    table_PARAMETER(select_PARAMETER(results))
+
 
 # modify the XML file
 # modify_kv=input('Input new attribute')
-modify_kv={"D":"This is concrete"}
-change_node_properties(results,modify_kv)
+modify_kv = {"D": "This is concrete"}
+change_node_properties(results, modify_kv)
 
-# new node
-new_node_kv = {"N":"Nu", "V":"0.2", "D":"Poisson's Ratio"}
-a = create_node("P", new_node_kv)
+# creat new node
+# new_node_tag = input('Input the tag of new node')
+# new_node_kv = input('Input the attributes of new node')
+new_node_tag = 'P'
+new_node_kv = {"N": "Nu", "V": "0.2", "D": "Poisson's Ratio"}
+a = create_node(new_node_tag, new_node_kv)
 add_child_node(results, a)
 
-# output results
-# new XML
-write_xml(tree,"./new.xml")
+# delete node
+# del_tag=input('Input the tag of the node to delete')
+# del_attribute=input('Input the attributes of the node to delete')
+del_tag = "P"
+del_attribute = {"N": "Fy"}
+del_node_by_tagkeyvalue(results, del_tag, del_attribute)
+
+# output results in new XML
+write_xml(tree, "./new.xml")
