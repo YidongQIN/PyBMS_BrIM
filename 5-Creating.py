e006a0204b0897af83c4c4be05b51c1bf7979f9b
YidongQIN <YidongQIN@users.noreply.github.com>
2017-12-15 12:05:14 -0500

    new and save OpenBrIM project

--- 5-Creating.py
+++ 5-Creating.py
@@ -7,7 +7,6 @@ def new_OpenBrIM(name):
     origin_string = '<O Alignment="None" N="new" T="Project" TransAlignRule="Right">\n    <O N="Units" T="Group">\n        <O Angle="Radian" Force="Kip" Length="Inch" N="Internal" T="Unit" Temperature="Fahrenheit" />\n        <O Angle="Degree" Force="Kip" Length="Feet" N="Geometry" T="Unit" Temperature="Fahrenheit" />\n        <O Angle="Degree" Force="Kip" Length="Inch" N="Property" T="Unit" Temperature="Fahrenheit" />\n    </O>\n    <O N="SW" T="AnalysisCase" WeightFactor="-1" />\n    <O Gravity="386.09" Modes="1" N="Seismic" T="AnalysisCaseEigen" />\n</O>'
     root = ET.fromstring(origin_string)
     root.attrib['N'] = name
-    print(root.attrib)
     return root
 
 
@@ -19,9 +18,9 @@ def save_OpenBrIM(root):
 
 # ----main----
 
+root = new_OpenBrIM('New_OpenBrIM_project')
 
-# tree = ob.read_xml('NEW_OpenBrIM.xml')
-# root = tree.getroot()
-root = new_OpenBrIM('The name of new OpenBrIM Project')
+results = ob.find_nodes(root, './O')
+ob.table_OBJECT(ob.select_OBJECT(results))
 
 save_OpenBrIM(root)
