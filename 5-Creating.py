9020d112b1da23b71524c78e514c909a150799b3
YidongQIN <YidongQIN@users.noreply.github.com>
2017-12-15 16:51:07 -0500

    new and save OpenBrIM project

--- 5-Creating.py
+++ 5-Creating.py
@@ -1,26 +1,31 @@
-import PyOpenBrIM as ob
 import xml.etree.ElementTree as ET
+import PyOpenBrIM as ob
 
+# create OBJECT or PARAMETER automatically
+# 之前的create node等可以作为参考。此处的newOP也是要作为ob程序包的一部分
 
-def new_OpenBrIM(name):
-    # default new OpenBrIM project with new name
-    origin_string = '<O Alignment="None" N="new" T="Project" TransAlignRule="Right">\n    <O N="Units" T="Group">\n        <O Angle="Radian" Force="Kip" Length="Inch" N="Internal" T="Unit" Temperature="Fahrenheit" />\n        <O Angle="Degree" Force="Kip" Length="Feet" N="Geometry" T="Unit" Temperature="Fahrenheit" />\n        <O Angle="Degree" Force="Kip" Length="Inch" N="Property" T="Unit" Temperature="Fahrenheit" />\n    </O>\n    <O N="SW" T="AnalysisCase" WeightFactor="-1" />\n    <O Gravity="386.09" Modes="1" N="Seismic" T="AnalysisCaseEigen" />\n</O>'
-    root = ET.fromstring(origin_string)
-    root.attrib['N'] = name
-    return root
+def str_to_dict(str):
+    pass
+# emp: <P N="z_height" V="108" UT="Length" Role="Input"/>
+# N and V is mandatory
+def new_P(N,V,Role,UT,UC,D,T=default):
+    pass
 
+'''emp of Object
+    <O N="Sections" T="Group">
+        <O N="Section_BottomChord" T="Section">
+'''
+# 参考xml的自动补齐？ O[N='a']>
 
-def save_OpenBrIM(root):
-    tree = ET.ElementTree(root)
-    out_path = root.attrib['N'] + '.xml'
-    tree.write(out_path, encoding="utf-8", xml_declaration=True)
+def new_O(T):
+    pass
 
-
+# object 需要创建模板，即上面是一个总体的，下面根据每种type编写模板化的
 # ----main----
 
-root = new_OpenBrIM('New_OpenBrIM_project')
+root = ob.new_OpenBrIM('New_OpenBrIM_project')
 
 results = ob.find_nodes(root, './O')
 ob.table_OBJECT(ob.select_OBJECT(results))
 
-save_OpenBrIM(root)
+ob.save_OpenBrIM(root)
--- PyOpenBrIM.py
+++ PyOpenBrIM.py
@@ -18,6 +18,22 @@ def write_xml(tree, out_path):
     tree.write(out_path, encoding="utf-8", xml_declaration=True)
 
 
+# create a new OpenBrIM project and name it
+def new_OpenBrIM(name):
+    # default new OpenBrIM project with new name
+    origin_string = '<O Alignment="None" N="new" T="Project" TransAlignRule="Right">\n    <O N="Units" T="Group">\n        <O Angle="Radian" Force="Kip" Length="Inch" N="Internal" T="Unit" Temperature="Fahrenheit" />\n        <O Angle="Degree" Force="Kip" Length="Feet" N="Geometry" T="Unit" Temperature="Fahrenheit" />\n        <O Angle="Degree" Force="Kip" Length="Inch" N="Property" T="Unit" Temperature="Fahrenheit" />\n    </O>\n    <O N="SW" T="AnalysisCase" WeightFactor="-1" />\n    <O Gravity="386.09" Modes="1" N="Seismic" T="AnalysisCaseEigen" />\n</O>'
+    root = ET.fromstring(origin_string)
+    root.attrib['N'] = name
+    return root
+
+
+# save the OpenBrIM model with the name in project attribute
+def save_OpenBrIM(root):
+    tree = ET.ElementTree(root)
+    out_path = root.attrib['N'] + '.xml'
+    tree.write(out_path, encoding="utf-8", xml_declaration=True)
+
+
 # search by path
 def find_nodes(tree, path):
     return tree.findall(path)
@@ -108,7 +124,7 @@ def other_attribute(dict):
 
 # pretty table
 def table_OBJECT(result_Object):
-    tb = pt.PrettyTable(["Name", "OBJECT Type", "Description","Other Attributes"])
+    tb = pt.PrettyTable(["Name", "OBJECT Type", "Description", "Other Attributes"])
     tb.align["Other Attributes"] = "l"
     for anode in result_Object:
         row = [anode.attrib.get("N"), anode.attrib.get("T"), anode.attrib.get("D"), other_attribute(anode.attrib)]
