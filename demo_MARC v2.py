51e0392a8711abb7cde4042da1570e76f2b0d5f4
YidongQIN <qinyidong87@outlook.com>
2018-03-21 14:35:52 -0400

    demo Marc OOP

--- demo_MARC v2.py
+++ demo_MARC v2.py
@@ -10,6 +10,31 @@ use ClassPyOpenBrIM to generate the xml file of MARC Bridge.
 from ClassPyOpenBrIM import *
 
 if __name__ == '__main__':
-    MARC=PyOpenBrIMElmt('MARC_OOP')
+    marc=PyOpenBrIMElmt('MARC_OOP')
+    # Units are in default template of new_project
+    marc.new_project()
+
+    # 1. Material
+
+
+    # 2. Sections
+
+
+    # 3. Structural Parameter
+
+
+    # 4. Points
+
+
+    # 5. Lines
+
+
+    # 6. Surfaces
+
+
+
+    # 7. Save and Show
+    ShowTree(marc)
+    marc.save_project()
 
 
