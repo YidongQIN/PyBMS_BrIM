7ca3f001682829523a7cfe962d997f3e6cddc4d0
YidongQIN <qinyidong87@outlook.com>
2018-05-08 16:32:35 -0400

    class Inspection

---
+++ PyInspection.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Yidong QIN'
+
+"""
+Bridge inspection information
+"""
+
+from PyOBobjects import *
+
+class Inspection(object):
+    def __init__(self):
+        pass
