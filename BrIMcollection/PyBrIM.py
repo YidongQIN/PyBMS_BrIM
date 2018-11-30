6a68efcc26ed8f76a272708942710e7d4f407d2d
YidongQIN <qinyidong87@outlook.com>
2018-11-30 13:43:46 -0500

    collections.UserDict

--- BrIMcollection/PyBrIM.py
+++ BrIMcollection/PyBrIM.py
@@ -7,6 +7,7 @@ __author__ = 'Yidong QIN'
 
 """
 import collections
+import json
 
 
 class PyBrIM(collections.UserDict):
@@ -16,41 +17,71 @@ class PyBrIM(collections.UserDict):
         self.type = brim_type
         super(PyBrIM, self).__init__(**brim_data)
 
-    @property
-    def brim(self):
-        """rename the UserDict.data{} as PyBrIM.brim{}"""
-        return self.data
+    def link(self, key: str, element):
+        if isinstance(element, PyBrIM):
+            self.__setitem__(key, element)
 
-    def __repr__(self):
-        return "{}_{}".format(self.type, self._id)
+    def __str__(self):
+        return "{}_{}\n  ->{}".format(self.type, self._id,
+                                      super(PyBrIM, self).__str__())
+
+    def jsondumps(self):
+        return json.dumps(self, indent=4, default=lambda obj: obj.__dict__)
 
     def __iter__(self):
         return iter(self.data.items())
-        # return iter(self.data.items())
+
+    # attributes can be used for getting information, but not recommended
+    def __setattr__(self, key, value):
+        if key in ('_id', 'type', 'data'):
+            # print('set default attr', key)
+            super(PyBrIM, self).__setattr__(key, value)
+        elif key in self.data.keys():
+            print('set attr in .data items', key)
+            super(PyBrIM, self).__setitem__(key, value)
+        else:
+            print("New attribute:", key, "=", value)
+            super(PyBrIM, self).__setitem__(key, value)
 
     def __getattr__(self, item):
         try:
             return self.data[item]
         except KeyError:
-            print("No attribute:", item)
+            print("No attribute found:", item)
             return
 
-    def __setattr__(self, key, value):
-        if key in ('_id', 'type', 'data'):
-            super(PyBrIM, self).__setattr__(key, value)
-        elif key in self.data.keys():
-            super(PyBrIM, self).__setitem__(key, value)
-        else:
-            print("New attribute:", key, "=", value)
-            #@TODO how to check if the new item is supported?
-            super(PyBrIM, self).__setitem__(key, value)
+    def __delattr__(self, item):
+        try:
+            super(PyBrIM, self).__delattr__(item)
+        except AttributeError:
+            try:
+                super(PyBrIM, self).__delitem__(item)
+            except KeyError:
+                print('Cannot find attribute to del:', item)
+
+
+class DocumentBrIM(PyBrIM):
+
+    def __init__(self, brim_id, brim_type, **brim_data):
+        super(DocumentBrIM, self).__init__(brim_id, brim_type, **brim_data)
+
+
+class EquipmentBrIM(PyBrIM):
+    pass
+
+
+class AbstractBrIM(PyBrIM):
+    pass
+
+
+class PhysicalBrIM(PyBrIM):
+    pass
 
 
 if __name__ == '__main__':
-    test = PyBrIM(1, 'test', section='sss', mat='material')
-    print(test.__dict__)
-    print(test.section)
-    test.section = "re sec"
-    test.wrong = "new section"
-    print(test.section)
-    print(test.__dict__)
+    mat = PyBrIM(2, 'material', a=100, fc=999)
+    # print(mat.jsondumps())
+    test = PyBrIM(1, 'non-type', section='SECT', material='MAT')
+    test.link('material', mat)
+    print(test)
+    print(test.jsondumps())
